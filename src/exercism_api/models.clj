b101c95a9cb193b9b9d0d1ad5dd3a805f0914311
Jason Lewis <jason@decomplecting.org>
2015-07-02 06:45:08 -0400

    Adding models

--- src/exercism_api/models.clj
+++ src/exercism_api/models.clj
@@ -2,7 +2,20 @@
   (require [schema.core :as s]))
 
 (s/defschema Track
-  {:id String
-   :slug String
-   :repository String
-   :active Boolean})
+  {:id s/Str
+   :slug s/Str
+   :repository s/Str
+   :language s/Str
+   :active s/Bool
+   :problems [s/Str]})
+
+(s/defschema Problem
+  {:id s/Str
+   :language s/Str
+   :track_id s/Str
+   :name s/Str
+   :slug s/Str
+   :files {
+           ;; TODO
+           }
+   :fresh s/Bool})
