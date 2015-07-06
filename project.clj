989a0ff3a5e8681a09d6064e6506102bdd86b4a8
Jason Lewis <jason@decomplecting.org>
2015-07-05 23:24:08 -0400

    Add model schemas... ish

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject exercism-api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "An API for Exercism.io"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-time "0.9.0"] ; required due to bug in lein-ring
                  [metosin/compojure-api "0.22.0"]
                  [pandect "0.5.2"]
-                 [environ "1.0.0"]]
+                 [environ "1.0.0"]
+                 [org.clojure/java.jdbc "0.3.7"]]
   :ring {:handler exercism-api.handler/app}
   :uberjar-name "server.jar"
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
--- src/exercism_api/models.clj
+++ src/exercism_api/models.clj
@@ -19,3 +19,49 @@
            ;; TODO
            }
    :fresh s/Bool})
+
+(s/defschema Comment
+  ;; STUB
+  {:id s/Integer
+   :user_id s/Num
+   :submission_id s/Num
+   :body s/Str
+   :html_body s/Str
+   })
+
+(s/defschema Submission
+  ;; STUB
+  {:id s/Num
+   :user_id s/Num
+   :key s/Str
+
+   :state s/Str
+   :language s/Str
+   :slug s/Str
+   :code s/Str
+   :done_at (s/maybe java.util.Date) ;; Should expand acceptable dt types
+   :is_liked s/Bool
+   :nit_count s/Num
+   :version s/Num
+   :user_exercise_id s/Num
+   :filename s/Str
+   })
+
+
+(s/defschema User
+  ;;STUB
+  {:id s/Num
+   :username s/Str
+   :email s/Str ;; TODO Email validation
+   :avatar_url s/Str ;; TODO URI validation
+   :github_id s/Num
+   :key s/Str
+
+   :mastery s/Str})
+
+(s/defschema Team
+  ;; STUB
+  {:id s/Num
+   :creator_id s/Num
+   :slug s/Str
+   :name s/Str})
